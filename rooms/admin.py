c85771c003090f6e01ff97f6ae98768cae2e1860
yaswanthsaivendra <yaswanthsaivendra@gmail.com>
2023-12-26 01:47:47 +0530

    resolved the empty message issue on clicking enter

--- rooms/admin.py
+++ rooms/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from rooms.models import Room
+from rooms.models import Room, Message
 
 # Register your models here.
 
 admin.site.register(Room)
+admin.site.register(Message)
